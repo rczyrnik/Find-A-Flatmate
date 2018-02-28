58437bb43679c2a6ad0e8a9f8a3a23f771d8bd5a
rczyrnik <rczyrnik@gmail.com>
2018-02-27 17:29:14 -0800

    some thorough lookseeing

--- my_pickle.py
+++ my_pickle.py
@@ -5,20 +5,23 @@ documentation!
 import pandas as pd
 import json
 
+start_of_path = '/Users/gandalf/Documents/data/'
+start_of_path = '/Users/gandalf/Documents/coding/data_do_not_commit/'
+
 def pickle_it(df,name):
-    filepath = '/Users/gandalf/Documents/data/'+name+'.pkl'
+    filepath = start_of_path+name+'.pkl'
     df.to_pickle(filepath)
 
 def unpickle_it(name):
-    filepath = '/Users/gandalf/Documents/data/'+name+'.pkl'
+    filepath = start_of_path+name+'.pkl'
     return pd.read_pickle(filepath)
 
 def json_it(df,name):
-    filepath = '/Users/gandalf/Documents/data/'+name+'.json'
+    filepath = start_of_path+name+'.json'
     df.to_json(filepath)
 
 def unjson_it(name):
-    filepath = '/Users/gandalf/Documents/data/'+name+'.json'
+    filepath = start_of_path+name+'.json'
     return pd.read_json(filepath)
 
 def my_other_to_datetime(x):
